6db23a450c4525e1dd3bb68cd8a7f73075d136c8
francisco-ramirez-ml <Francisco.Ramirez@Xoriant.Com>
2024-07-26 18:57:22 -0700

    update provider

--- provider.tf
+++ provider.tf
@@ -1,14 +1,18 @@
 terraform {
- required_providers {
-   aws = {
-     source  = "hashicorp/aws"
-     version = "~> 4.18.0"
-   }
- }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.18.0"
+    }
+  }
 
   backend "s3" {
     bucket = "task-5-terraform-state"
     key = "terraform-states/tf-jenkins.tfstate"
     region = "us-west-2"
   }
+}
+
+provider "aws" {
+  region = "us-west-2"
 }
